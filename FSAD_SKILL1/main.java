2a386382c054b80990f9211f38cb26d7ee9c48a3
shubhamanand020 <anandshubham906@gmail.com>
2026-03-14 21:10:43 +0530

    first commit

---
+++ FSAD_SKILL1/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git Commands!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("This is line number: " + i);
+        }
+    }
+
+}
