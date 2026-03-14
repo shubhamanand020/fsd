e537105076d6885cb8bf5e1cdddef2a6f311328d
Shubham Anand <2400032642@kluniversity.in>
2026-03-14 21:13:21 +0530

    .

--- FSAD_SKILL1/main.java
+++ FSAD_SKILL1/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git Commands!");
+        System.out.println("Git Commands!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("This is line number: " + i);
         }
